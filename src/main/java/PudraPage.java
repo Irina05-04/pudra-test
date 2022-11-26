46d35ad7bb98edd3f5c9a9ce4c81073dfde2fbce
irina <irisha.dieriecha@mail.ru>
2022-11-26 15:05:20 +0300

    feat: add login test

--- src/main/java/PudraPage.java
+++ src/main/java/PudraPage.java
@@ -13,8 +13,27 @@ public class PudraPage {
     private String xpathResults = "//a[@class='product__title']";
     private String xpathProduct = "//form[@name='product_form_411']";
     private String xpathProductCode = "//span[@id='product_code_411']";
+
+    private String xpathpRrofileBtn = "//div[@class='profile__btn ']";
+    private String xpathEnterBtn = "//a[@data-ca-target-id='login_block917']";
+    private String xpathSubmitForm = "//button[@name='dispatch[auth.login]']";
+    private String xpathErrorMailRequire = "//span[@id='login_popup917_error_message']";
+    private String xpathErrorPasswordRequire = "//span[@id='psw_popup917_error_message']";
+    private String xpathLogin = "//input[@name='user_login']";
+    private String xpathPsw = "//input[@name='password']";
+    private String xpathEncorrectDate = "//div[@class='ty-login-form__wrong-credentials-container']";
+    private String xpathMyProfile = "//li[@class='ty-account-info__item  ty-account-info__name ty-dropdown-box__item']";
+
     public final String RESULT_SEARCH_TEXT = "Крем для рук FarmStay с черным жемчугом Hand Cream Black Pearl - 100 гр";
     public final String RESULT_PRODUCT_CODE = "J0001S";
+
+    public final String RESULT_ERROR_MAIL_REQUIRE = "Поле E-mail обязательное.";
+    public final String RESULT_ERROR_PSW_REQUIRE = "Поле Пароль обязательное.";
+    public final String RESULT_ENCORRECT_DATE = "Вы ввели неверный логин или пароль. Пожалуйста, попробуйте еще раз.";
+    public final String CORRECT_LOGIN = "irisha.dieriecha@mail.ru";
+    public final String CORRECT_PSW = "1z2x3c4v5b6n";
+    public final String CORRECT_RESULTS = "Ирина Дереча";
+
     public PudraPage(WebDriver driver){
         this.driver = driver;
     }
@@ -43,10 +62,75 @@ public class PudraPage {
         WebElement elementProductClick = driver.findElement(byProductResult);
         elementProductClick.click();
     }
-    public String checkProductCode() {
-        clickProduct();
+    public String findProductCode(){
         By byProductCodeResult = By.xpath(xpathProductCode);
         WebElement elementProductCode = driver.findElement(byProductCodeResult);
         return elementProductCode.getText();
+    }
+    public String checkProductCode() {
+        clickProduct();
+        return findProductCode();
+    }
+
+    public void clickProfileBtn() {
+        By byProfileBtn = By.xpath(xpathpRrofileBtn);
+        WebElement elementProfileBtn = driver.findElement(byProfileBtn);
+        elementProfileBtn.click();
+    }
+    public void clickEnterBtn() {
+        By byEnterBtn = By.xpath(xpathEnterBtn);
+        WebElement elementEnterBtn = driver.findElement(byEnterBtn);
+        elementEnterBtn.click();
+    }
+    public void clickEnterSubmit() {
+        By bySubmitForm = By.xpath(xpathSubmitForm);
+        WebElement elementSubmitForm = driver.findElement(bySubmitForm);
+        elementSubmitForm.click();
+    }
+    public void goLogIn() {
+        clickProfileBtn();
+        clickEnterBtn();
+    }
+    public void checkEmptyFields() {
+        goLogIn();
+        clickEnterSubmit();
+    }
+    public String findErrorEmailRequire() {
+        By byErrorMailRequire = By.xpath(xpathErrorMailRequire);
+        WebElement elementErrorMailRequire = driver.findElement(byErrorMailRequire);
+        return elementErrorMailRequire.getText();
+    }
+    public String findErrorPasswordRequire() {
+        By byErrorPswRequire = By.xpath(xpathErrorPasswordRequire);
+        WebElement elementErrorPswRequire = driver.findElement(byErrorPswRequire);
+        return elementErrorPswRequire.getText();
+    }
+
+    public void printLogin(String login) {
+        By byLoginInput = By.xpath(xpathLogin);
+        WebElement elementLoginInput= driver.findElement(byLoginInput);
+        elementLoginInput.sendKeys(login);
+    }
+    public void printPsw(String password) {
+        By byPswInput = By.xpath(xpathPsw);
+        WebElement elementPswInput = driver.findElement(byPswInput);
+        elementPswInput.sendKeys(password);
+    }
+    public void SubmitDate(String login, String password) {
+        goLogIn();
+        printLogin(login);
+        printPsw(password);
+        clickEnterSubmit();
+    }
+    public String findErrorMessageEncorrectDate(){
+        By byErrorMessageEncorrectDate = By.xpath(xpathEncorrectDate);
+        WebElement elementErrorMessageEncorrectDate = driver.findElement(byErrorMessageEncorrectDate);
+        return elementErrorMessageEncorrectDate.getText();
+    }
+
+    public String findMyProfile(){
+        By byMyProfile = By.xpath(xpathMyProfile);
+        WebElement elementMyProfile = driver.findElement(byMyProfile);
+        return elementMyProfile.getText();
     }
 }
--- src/test/java/PudraTest.java
+++ src/test/java/PudraTest.java
@@ -45,6 +45,38 @@ public class PudraTest {
         // TODO THEN
         Assert.assertEquals(page.RESULT_PRODUCT_CODE, actual);
     }
+    @Test
+    public void emptyFildsRegistr() {
+
+        // TODO WHEN
+        page.checkEmptyFields();
+        String actual = page.findErrorEmailRequire();
+        String actualPsw = page.findErrorPasswordRequire();
+
+        // TODO THEN
+        Assert.assertEquals(page.RESULT_ERROR_MAIL_REQUIRE, actual);
+        Assert.assertEquals(page.RESULT_ERROR_PSW_REQUIRE, actualPsw);
+    }
+    @Test
+    public void EncorrectValue() {
+
+        // TODO WHEN
+        page.SubmitDate("lala@gmail.com", "01234");
+        String actual = page.findErrorMessageEncorrectDate();
 
 
+        // TODO THEN
+        Assert.assertEquals(page.RESULT_ENCORRECT_DATE, actual);
+    }
+    @Test
+    public void CorrectValue() {
+
+        // TODO WHEN
+        page.SubmitDate(page.CORRECT_LOGIN , page.CORRECT_PSW);
+        page.clickProfileBtn();
+        String actual = page.findMyProfile();
+
+        // TODO THEN
+        Assert.assertEquals(page.CORRECT_RESULTS, actual);
+    }
 }
